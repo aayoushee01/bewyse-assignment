519a4aabb225915b74be4e98827e6eeaf9821e0a
Chandan Ahire <aayoushee.ahlawat@gmail.com>
2023-11-04 17:44:09 +0530

    url endpoint

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('myapp/register/', views.register),
-    path('myapp/login/', views.login),
-    path('myapp/profile/view/', views.view_profile),
-    path('myapp/profile/edit/', views.edit_profile),
+    path('accounts/register/', views.register),
+    path('accounts/login/', views.login),
+    path('accounts/profile/view/', views.view_profile),
+    path('accounts/profile/edit/', views.edit_profile),
 ]
